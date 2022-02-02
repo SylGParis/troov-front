f7fe57f053a178c3edef64919f5a544c5cb7ee7a
SylGParis <sylviane.gaonach@gmail.com>
2022-02-02 17:35:31 +0100

    add card component

--- pages/objectScreen.vue
+++ pages/objectScreen.vue
@@ -1,5 +1,18 @@
 
 
 <template>
-  <NuxtLink to="/">Home page</NuxtLink>
+
+  <div>
+    <NuxtLink to="/">Home page</NuxtLink>
+    <b-container fluid class="bv-example-row">
+      <b-row class="justify-content-md-center">
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        
+      </b-row>
+    </b-container>
+
+    <ObjectCard />
+    
+</div>
 </template>
